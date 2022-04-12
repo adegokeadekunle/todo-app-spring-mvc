1781484bd349ad399c8e50da2288dc2f30a2518a
atumar <almustapha.tukur@decagon.dev>
2022-04-12 12:26:37 +0100

    correcting routes

--- src/main/java/com/group5/activitytrackergroup5task/controllers/PersonController.java
+++ src/main/java/com/group5/activitytrackergroup5task/controllers/PersonController.java
@@ -1,4 +1,22 @@
 package com.group5.activitytrackergroup5task.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PersonController {
+    @GetMapping("/")
+    public String displaySignin(){
+        return "index";
+    }
+
+    @GetMapping("/signup")
+    public String displaySignup(){
+        return "signup";
+    }
+
+    @GetMapping("/login")
+    public String displayLogin(){
+        return "redirect:/";
+    }
 }
