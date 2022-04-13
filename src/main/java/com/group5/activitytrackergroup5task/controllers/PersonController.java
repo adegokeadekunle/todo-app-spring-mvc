1c970214f27421e0cc77f0e9ddcfb394b3695355
atumar <almustapha.tukur@decagon.dev>
2022-04-13 15:30:19 +0100

    binding task page with task api

--- src/main/java/com/group5/activitytrackergroup5task/controllers/PersonController.java
+++ src/main/java/com/group5/activitytrackergroup5task/controllers/PersonController.java
@@ -1,10 +1,27 @@
 package com.group5.activitytrackergroup5task.controllers;
 
+import com.group5.activitytrackergroup5task.models.Todo;
+import com.group5.activitytrackergroup5task.services.PersonService;
+import com.group5.activitytrackergroup5task.services.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class PersonController {
+
+    private final TodoService todoService;
+    private final PersonService personService;
+
+    @Autowired
+    public PersonController(TodoService todoService, PersonService personService) {
+        this.todoService = todoService;
+        this.personService = personService;
+    }
+
     @GetMapping("/")
     public String displaySignin(){
         return "index";
@@ -21,7 +38,14 @@ public class PersonController {
     }
 
     @GetMapping("/home")
-    public String displayTracker(){
+    public String displayTracker(Model model){
+        Todo todo = new Todo();
+        List<Todo> todos = todoService.getTodoList();
+//        int completed = ;
+//        int inprogress = ;
+            model.addAttribute("todo", todo);
+            model.addAttribute("todos", todos);
+
         return "tracker";
     }
 }
--- src/main/java/com/group5/activitytrackergroup5task/models/Person.java
+++ src/main/java/com/group5/activitytrackergroup5task/models/Person.java
@@ -1,5 +1,15 @@
 package com.group5.activitytrackergroup5task.models;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
 public class Person {
+    @Id
     private Long id;
+
+    @OneToMany
+    List<Todo> todos;
 }
