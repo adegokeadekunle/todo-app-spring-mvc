dc7fac8ef5d9f770d4b637d7f3e12f2e21c4a7ca
mac <mac@macs-MacBook-Air.local>
2022-04-13 15:30:36 +0100

    finished code

--- src/main/java/com/group5/activitytrackergroup5task/controllers/PersonController.java
+++ src/main/java/com/group5/activitytrackergroup5task/controllers/PersonController.java
@@ -1,22 +1,57 @@
 package com.group5.activitytrackergroup5task.controllers;
 
+import com.group5.activitytrackergroup5task.dtos.LoginDto;
+import com.group5.activitytrackergroup5task.models.Person;
+import com.group5.activitytrackergroup5task.services.PersonService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class PersonController {
+
+    private final PersonService personService;
+
+    public PersonController(PersonService personService){
+        this.personService = personService;
+    }
+
     @GetMapping("/")
-    public String displaySignin(){
-        return "index";
+    public String login (Model model){
+        LoginDto loginDto = new LoginDto();
+        model.addAttribute("loginDto", loginDto);
+        return "";
     }
 
-    @GetMapping("/signup")
-    public String displaySignup(){
-        return "signup";
-    }
+    @PostMapping("/")
+    public String login(@ModelAttribute("loginDto") LoginDto loginDto, HttpSession session){
+        Person person = personService.login(loginDto);
+        if (person != null){
+            session.setAttribute("user", person);
+            return "redirect:/home";
+        }
 
-    @GetMapping("/login")
-    public String displayLogin(){
         return "redirect:/";
     }
+
+
+
+//    @GetMapping("/")
+//    public String displaySignin(){
+//        return "index";
+//    }
+//
+//    @GetMapping("/signup")
+//    public String displaySignup(){
+//        return "signup";
+//    }
+//
+//    @GetMapping("/login")
+//    public String displayLogin(){
+//        return "redirect:/";
+//    }
 }
--- src/main/java/com/group5/activitytrackergroup5task/models/Person.java
+++ src/main/java/com/group5/activitytrackergroup5task/models/Person.java
@@ -1,4 +1,39 @@
 package com.group5.activitytrackergroup5task.models;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Email
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    @Size(min = 8, max = 12)
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime timeCreated;
 }
